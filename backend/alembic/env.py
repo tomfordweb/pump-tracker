b44b2eb2afaeeeaf67be66671c2c913e8d26b9c9
Tom Ford <tom@datacompusa.com>
2021-12-25 14:33:28 -0500

    create migrations for existing tables

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,9 +1,8 @@
 from logging.config import fileConfig
 
 from alembic import context
+from app.database import Base
 from sqlalchemy import engine_from_config, pool
-
-from ..app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from . import models
 from .database import engine
 from .routers import users, workouts
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(users.router)
