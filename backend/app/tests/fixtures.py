b0ec596ab0267d37958256956e0dc1715d0c7216
Tom Ford <tom@datacompusa.com>
2021-12-25 17:09:37 -0500

    share fixtures removing a lot of duplicate code

--- backend/app/tests/fixtures.py
+++ backend/app/tests/fixtures.py
@@ -1,8 +1,13 @@
 import pytest
 
-from .app import TestingSessionLocal
+from .app import TestingSessionLocal, client
 
-
+TESTING_ACCOUNT_DETAILS = {
+    "email": "deadpool@example.com", 
+    "username": "Deadpool", 
+    "password1": "secret", 
+    "password2": "secret"
+}
 @pytest.fixture
 def truncate_database():
     session = TestingSessionLocal()
@@ -10,3 +15,30 @@ def truncate_database():
     session.execute('''DELETE FROM workouts''')
     session.commit()
     session.close()
+
+@pytest.fixture
+def create_testing_account():
+    response = client.post(
+        "/users",
+        json=TESTING_ACCOUNT_DETAILS
+    )
+    return response
+
+@pytest.fixture
+def create_alternate_testing_account():
+    pass
+
+
+@pytest.fixture
+def create_access_token_for_user(create_testing_account):
+    account = create_testing_account
+    account_json = account.json()
+    response = client.post(
+        '/token',
+        data={
+            "username": TESTING_ACCOUNT_DETAILS.get('username'), 
+            "password": TESTING_ACCOUNT_DETAILS.get('password1')
+        },
+        headers={'Content-Type': 'application/x-www-form-urlencoded'}
+    )
+    return response
--- backend/app/tests/routers/test_users.py
+++ backend/app/tests/routers/test_users.py
@@ -8,35 +8,26 @@ from ...database import Base
 from ...dependencies import get_db
 from ...main import app
 from ..app import TestingSessionLocal, client
-from ..fixtures import truncate_database
+from ..fixtures import (TESTING_ACCOUNT_DETAILS, create_access_token_for_user,
+                        create_testing_account, truncate_database)
 
 
-def test_create_user(truncate_database):
-    response = client.post(
-        "/users",
-        json={"email": "deadpool@example.com", "username": "deadpool", "password1": "chimichangas4life", "password2": "chimichangas4life"},
-    )
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data["email"] == "deadpool@example.com"
+def test_create_user(truncate_database, create_testing_account):
+    assert create_testing_account.status_code == 200
+    data = create_testing_account.json()
+    assert data["email"] == TESTING_ACCOUNT_DETAILS.get('email')
     assert "id" in data
-    assert "password" not in response.text
-    user_id = data["id"]
+    assert "password" not in create_testing_account.text
 
-def test_create_user_rejects_duplicate_username(truncate_database):
-    username="deadpool2"
-    response = client.post(
-        "/users",
-        json={"email": "deadpool@example.com", "username": username, "password1": "chimichangas4life", "password2": "chimichangas4life"},
-    )
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data["email"] == "deadpool@example.com"
-    assert "id" in data
+def test_create_user_rejects_duplicate_username(truncate_database, create_testing_account):
+    assert create_testing_account.status_code == 200
 
     response = client.post(
         "/users",
-        json={"email": "deadpool+alternate@example.com", "username": username, "password1": "chimichangas4life", "password2": "chimichangas4life"},
+        json={
+            **TESTING_ACCOUNT_DETAILS,
+            **{"email": "someUniqueEmail@example.com"}, 
+        },
     )
     assert response.status_code == 400
     assert "username" in response.text
@@ -45,7 +36,7 @@ def test_create_user_rejects_duplicate_username(truncate_database):
 def test_create_user_only_accepts_alphanumeric_username(truncate_database):
     response = client.post(
         "/users",
-        json={"email": "john@example.com", "username": "!@#$**#*#(", "password1": "chimichangas4life", "password2": "chimichangas4life"},
+        json={**TESTING_ACCOUNT_DETAILS, **{"username": "!@#$**#*#("}},
     )
     assert response.status_code == 422
     assert "alphanumeric" in response.text
@@ -53,46 +44,21 @@ def test_create_user_only_accepts_alphanumeric_username(truncate_database):
 def test_create_user_rejects_emails_that_are_not_emails(truncate_database):
     response = client.post(
         "/users",
-        json={"email": "asdf", "username": "tomtom", "password1": "chimichangas4life", "password2": "chimichangas4life"},
+        json={**TESTING_ACCOUNT_DETAILS, **{"email": "asdf"}},
     )
     assert response.status_code == 422
     assert "value_error.emailsyntax" in response.text
 
 
-def test_you_can_get_a_token(truncate_database):
-    username="deadpool"
-    password="secret"
-    response = client.post(
-        "/users",
-        json={"email": "deadpool@example.com", "username": username, "password1": password, "password2": password},
-    )
-    assert response.status_code == 200, response.text
-
-    response = client.post(
-            '/token',
-            data={"username": username, "password": password},
-            headers={'Content-Type': 'application/x-www-form-urlencoded'}
-    )
-    assert response.status_code == 200
-    assert "token" in response.text
+def test_you_can_get_a_token(truncate_database, create_access_token_for_user):
+    assert create_access_token_for_user.status_code == 200
+    assert "token" in create_access_token_for_user.text
 
 
-def test_you_can_hit_the_user_healthcheck_route(truncate_database):
-    username="deadpool"
-    password="secret"
-    response = client.post(
-        "/users",
-        json={"email": "deadpool@example.com", "username": username, "password1": password, "password2": password},
-    )
-    assert response.status_code == 200, response.text
+def test_you_can_hit_the_user_healthcheck_route(truncate_database, create_access_token_for_user):
+    assert create_access_token_for_user.status_code == 200
+    token = create_access_token_for_user.json().get('access_token')
 
-    response = client.post(
-            '/token',
-            data={"username": username, "password": password},
-            headers={'Content-Type': 'application/x-www-form-urlencoded'}
-    )
-    assert response.status_code == 200
-    token = response.json().get('access_token')
     assert token is not None
 
     response = client.get(
@@ -104,23 +70,10 @@ def test_you_can_hit_the_user_healthcheck_route(truncate_database):
     assert "password" not in response.text
 
 
-
-
-
-def test_create_user_rejects_duplicate_email(truncate_database):
-    email="deadpool@example.com"
+def test_create_user_rejects_duplicate_email(truncate_database, create_testing_account):
     response = client.post(
         "/users",
-        json={"email": email, "username": "test1", "password1": "chimichangas4life", "password2": "chimichangas4life"},
-    )
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data["email"] == "deadpool@example.com"
-    assert "id" in data
-
-    response = client.post(
-        "/users",
-        json={"email": email, "username": "test2", "password1": "chimichangas4life", "password2": "chimichangas4life"},
+        json={**TESTING_ACCOUNT_DETAILS, **{"username": "somethingtrulyunique"}},
     )
     assert response.status_code == 400
     assert "email" in response.text
