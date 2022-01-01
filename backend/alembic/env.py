aaa5092ba8613be1a17b0aa541b3e7b76d04e4bc
Tom Ford <tom@datacompusa.com>
2022-01-01 08:04:58 -0500

    fix migrations

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -16,6 +16,8 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from app.models import Plan, User, Workout
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
